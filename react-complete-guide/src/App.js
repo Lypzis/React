098449ecf015bcccaaed35f06bf3724d8fbd92cf
Lypzis <victorvp2008@hotmail.com>
2019-04-30 12:04:21 -0300

    Updating App.js, Person.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,24 +13,18 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 },
     ],
-    otherState: 'some other value hey'
+    otherState: 'some other value hey',
+    showPersons: false
   }
 
-  switchNameHandler = newName => {
-    //console.log('I have been clicked :v');
-
-    // don't do this this.state.person[0].name = 'Maximilian';
-
-    // will only update 'person' state
-    this.setState({
-      person: [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
+  deletePersonHandler = personIndex => {
+    //const persons = this.state.person.slice(); // slice without argument will simply make a copy of the array, good practice :D
+    // or use spread operator to make a copy with the objects of the older array, creating a new one
+    const persons = [...this.state.person]; // only for arrays!
+    persons.splice(personIndex, 1); // removes the element of respective index, from the copy array
+    this.setState({person: persons}); // updates state with the new array
   }
- 
+
   nameChangedHandler = event => {
     this.setState({
       person: [
@@ -41,41 +35,13 @@ class App extends Component {
     });
   }
 
-  // array destructuring
-  /* if turning this class into a function 'hook'
-  const [ personsState, setPersonsState] = useState({
-    // array of objects
-    person: [
-      { name: "Max", age: 28 },
-      { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 },
-    ],
-    otherState: 'some other value hey'
-  });
+  // if showing, set to false, else to true, denying itself
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
 
-
-  const [ otherState, setOtherState ] = useState('My other state');
-
-  console.log(personsState, otherState);
-
-  console.log(personsState);
-
-  const switchNameHandler = () => {
-    //console.log('I have been clicked :v');
-  
-    // don't do this this.state.person[0].name = 'Maximilian';
-  
-    // old state will be replaced by this, important note!
-    setPersonsState({
-      person: [
-        { name: "Maximilian", age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 27 }
-      ],
-      otherState: personsState.otherState
-    });
-  } */
-
+  // Render elements to the dom
   render() {
     // inline style
     const style = {
@@ -86,6 +52,20 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+    // the persons list block, default is null
+    if (this.state.showPersons)
+      persons = (
+        <div>
+          {this.state.person.map((person, index) => {
+            return <Person
+              click={this.deletePersonHandler.bind(this, index)}
+              name={person.name}
+              age={person.age} />
+          })}
+        </div>
+      );
+
     return (
       // this is not html, it's .jsx :D
       // use 'bind' and never 'this.switchNameHandler('Marx')' 
@@ -93,16 +73,12 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!! :O</p>
-        
-        <button style={style} onClick={() => this.switchNameHandler('Unknown')}>Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-          changed={this.nameChangedHandler}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-        >My Hobbies: Racing.</Person>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {
+          // cannot use if statements inside here, so this is how its done, shows list if 'true',
+          // else, shows nothing ('null')
+          persons
+        }
       </div>
     );
   }
