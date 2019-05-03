c8939410469f13c1c2abb8d19831d97ac611d420
Lypzis <victorvp2008@hotmail.com>
2019-05-03 10:52:01 -0300

    Update App.js, App.css

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
   state = {
     // array of objects
     person: [
-        {id: 'aaa', name: 'Max', age: 28 },
-        {id: 'aab', name: 'Manu', age: 29 },
-        {id: 'abb', name: 'Stephanie', age: 27 }
+      { id: 'aaa', name: 'Max', age: 28 },
+      { id: 'aab', name: 'Manu', age: 29 },
+      { id: 'abb', name: 'Stephanie', age: 27 }
     ],
     otherState: 'some other value hey',
     showPersons: false
@@ -22,12 +22,12 @@ class App extends Component {
     // or use spread operator to make a copy with the objects of the older array, creating a new one
     const persons = [...this.state.person]; // only for arrays!
     persons.splice(personIndex, 1); // removes the element of respective index, from the copy array
-    this.setState({person: persons}); // updates state with the new array
+    this.setState({ person: persons }); // updates state with the new array
   }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.person.findIndex(person => person.id === id); // find respective person id
-   
+
     //const person = {...this.state.person[personIndex]}; // makes a copy of the respective object
     //person.name = event.target.value; // change the respective object name by the target value
     //const persons = [...this.state.person]; // copy state persons array
@@ -36,7 +36,7 @@ class App extends Component {
     // OR
     const persons = [...this.state.person]; // copy state person array
     persons[personIndex].name = event.target.value; // change the respective person object name from the copy array by the target value
-    
+
     this.setState({ person: persons }); // update state with the new information
   }
 
@@ -50,16 +50,18 @@ class App extends Component {
   render() {
     // inline style
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
+      backgroundColor: 'green',
+      color: '#fff',
+      fontSize: '2rem',
       border: '1px solid blue',
       padding: '.8rem',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      outline: 'none'
     }
 
     let persons = null;
     // the persons list block, default is null
-    if (this.state.showPersons)
+    if (this.state.showPersons) {
       persons = ( //always use map (the array function, not Map, if you know what I mean ;) ) to render lists
         <div>
           {this.state.person.map((person, index) => {
@@ -67,11 +69,27 @@ class App extends Component {
               key={person.id}
               click={this.deletePersonHandler.bind(this, index)}
               name={person.name}
-              age={person.age} 
-              changed={event => this.nameChangedHandler(event, person.id)}/>
+              age={person.age}
+              changed={event => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
+
+      // dinamically changes background of the button, which is the one recieving the inline style object
+      style.backgroundColor = 'red';
+    }
+
+    // creating a class list
+    let classes = [];
+
+    // dinamic classes
+    if (this.state.person.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.person.length < 2) {
+      classes.push('bold');
+    }
 
     return (
       // this is not html, it's .jsx :D
@@ -79,7 +97,7 @@ class App extends Component {
       // alternativelly to bind uses: '() => this.switchNameHandler()', though, not best practice
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!!! :O</p>
+        <p className={classes.join(' ')}>This is really working!!! :O</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {
           // cannot use if statements inside here, so this is how its done, shows list if 'true',
