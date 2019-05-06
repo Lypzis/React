71476f0ebf0ff52ec1513d7ee04a6b4026e48098
Lypzis <victorvp2008@hotmail.com>
2019-05-06 16:48:20 -0300

    Updates

---
+++ precache-manifest.10deb0858ea5f280264bfe0aef0a7089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be756e60718c7540ecab9b086b370e4",
+    "url": "/React/react-complete-guide/build/index.html"
+  },
+  {
+    "revision": "b3e1aeed8c826b4991f5",
+    "url": "/React/react-complete-guide/build/static/css/main.cc338190.chunk.css"
+  },
+  {
+    "revision": "d51313e0c2a5da14b3ff",
+    "url": "/React/react-complete-guide/build/static/js/2.fcd46767.chunk.js"
+  },
+  {
+    "revision": "b3e1aeed8c826b4991f5",
+    "url": "/React/react-complete-guide/build/static/js/main.e05929cc.chunk.js"
+  },
+  {
+    "revision": "ab76404c85f57483405f",
+    "url": "/React/react-complete-guide/build/static/js/runtime~main.849f62bb.js"
+  }
+]);
